b4310bced4bfb8b76420ccae07d6732068cd14ef
Vlad <tu4invladislav2511@gmail.com>
2021-08-28 07:18:20 +0300

    Push full project
    Push full project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+cubeAngle = 90;
+document.getElementById("cube").addEventListener("click", rotate);
+
+function rotate(){
+    cube.style.transform = "rotateY(" + cubeAngle +       "deg)";
+    if (cubeAngle >= 90) {
+        cubeAngle = cubeAngle + 90;
+    }  
+}
